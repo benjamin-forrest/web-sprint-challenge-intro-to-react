e3ba45cf32f9d7bf580a3948d0baae18fefafa1e
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-02-18 22:45:42 -0800

    star wars image and app.js maps, character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,35 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Divider = styled.div`
+    .card {
+        background: white;
+        display: flex;
+        width:60%;
+        padding: 8px;
+        border-radius: 10px;
+        flex-direction: column;
+        justify-content: space-between;
+        color: red;
+        margin: auto;
+        margin-bottom: 5px;
+        margin-top:5px;
+    }
+    
+` 
+
+function Character(props) {
+    const {data}=props;
+  return (
+    <Divider>
+    <div className='card-border'>
+    <div className='card'>
+      <p>{data.name} is {data.height} cm tall.</p>
+    </div>
+    </div>
+    </Divider>
+  )
+}
+
+export default Character;
